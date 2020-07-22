12f4319424178518aaabdb0029fea5c8cb4f5e19
Dellil <aksaksdm@naver.com>
2020-07-22 16:29:36 +0900

    has upgraded electron version 6 to 9

--- front/src/App.js
+++ front/src/App.js
@@ -5,32 +5,31 @@ import styled from "styled-components";
 const { app } = window.require("electron").remote;
 
 const StyledBtn = styled.button`
-    height: 35px;
-    background: black;
-    color: white;
-    border: none;
-    border-radius: 5px;
+	height: 35px;
+	background: black;
+	color: white;
+	border: none;
+	border-radius: 5px;
 `;
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    
-                    <h2>React Redux Electron</h2>
-                </div>
-                <p className="App-intro">
-                    <b> Release </b>
-                    Version: {app.getVersion()}
-                </p>
-                <Button variant="contained" color="primary">
-                    hello world
-                </Button>
-                <StyledBtn>StyledComponents</StyledBtn>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<h2>React Redux Electron</h2>
+				</div>
+				<p className="App-intro">
+					<b> Release </b>
+					Version: {app.getVersion()}
+				</p>
+				<Button variant="contained" color="primary">
+					hello world devtool열고싶어 시발
+				</Button>
+				<StyledBtn>StyledComponents</StyledBtn>
+			</div>
+		);
+	}
 }
 
 export default App;
