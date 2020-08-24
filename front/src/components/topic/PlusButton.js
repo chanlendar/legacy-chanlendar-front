95fb675983732441d806171ffcdc81685197613f
Dellil <aksaksdm@naver.com>
2020-08-24 17:49:06 +0900

    add TopicModal

--- front/src/components/topic/PlusButton.js
+++ front/src/components/topic/PlusButton.js
@@ -1,36 +1,35 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Avatar, Button, Grid, TextField } from "@material-ui/core";
+import { Avatar, Button, Grid } from "@material-ui/core";
 
 import {
 	OPEN_TOPIC_MODAL_EVENT,
 	CLOSE_TOPIC_MODAL_EVENT,
 	ADD_TOPIC_EVENT,
 } from "../../reducers";
+import { useOpenAndCloseEvent, useInput } from "../../hooks";
+import TopicModal from "./TopicModal";
 import {
 	useTopicStyles,
 	useTopicButtonStyles,
 	CustomTooltip,
-	useModalStyles,
 } from "../../styles";
-import CustomModal from "../CustomModal";
-import { useOpenAndCloseEvent, useInput } from "../../hooks";
 
 function PlusButton() {
 	const topicStyles = useTopicStyles();
 	const buttonStyles = useTopicButtonStyles();
-	const modalStyles = useModalStyles();
 
 	const isOpend = useSelector((state) => state.isTopicModalOpend);
 	const avatarVariant = isOpend ? "rounded" : "circle";
 
 	const [input, onInputChange] = useInput("");
 	const dispatch = useDispatch();
+
 	const onCreateClick = (e) => {
 		dispatch({ type: ADD_TOPIC_EVENT, data: input });
 	};
 
-	const [openEvent, closeEvent] = useOpenAndCloseEvent(
+	const [onOpenEvent, onCloseEvent] = useOpenAndCloseEvent(
 		OPEN_TOPIC_MODAL_EVENT,
 		CLOSE_TOPIC_MODAL_EVENT,
 	);
@@ -39,39 +38,17 @@ function PlusButton() {
 		<Grid className={topicStyles.root} item>
 			<CustomTooltip title="토픽 추가하기" placement="right" arrow>
 				<Avatar className={topicStyles.circle} variant={avatarVariant}>
-					<Button className={buttonStyles.root} onClick={openEvent}>
+					<Button className={buttonStyles.root} onClick={onOpenEvent}>
 						+
 					</Button>
 				</Avatar>
 			</CustomTooltip>
-			<CustomModal
-				title="토픽 만들기"
-				description="어떤 주제인가요?"
+			<TopicModal
 				isOpend={isOpend}
-				onCloseClick={closeEvent}
-			>
-				<form>
-					<TextField
-						variant="filled"
-						label="토픽이름"
-						autoFocus
-						fullWidth
-						onChange={onInputChange}
-					/>
-				</form>
-				<div className={modalStyles.buttons}>
-					<Button
-						variant="outlined"
-						onClick={closeEvent}
-						style={{ marginRight: "10px" }}
-					>
-						X
-					</Button>
-					<Button variant="contained" onClick={onCreateClick}>
-						만들기
-					</Button>
-				</div>
-			</CustomModal>
+				onCreateClick={onCreateClick}
+				onCloseEvent={onCloseEvent}
+				onInputChange={onInputChange}
+			/>
 		</Grid>
 	);
 }
---
+++ front/src/components/topic/TopicModal.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button, TextField } from "@material-ui/core";
+
+import CustomModal from "../CustomModal";
+import { useModalStyles } from "../../styles";
+
+function TopicModal({isOpend, onCreateClick, onCloseEvent, onInputChange}) {
+    const modalStyles = useModalStyles();
+
+	return (
+		<CustomModal
+			title="토픽 만들기"
+			description="어떤 주제인가요?"
+			isOpend={isOpend}
+			onCloseClick={onCloseEvent}
+		>
+			<form>
+				<TextField
+					variant="filled"
+					label="토픽이름"
+					autoFocus
+					fullWidth
+					onChange={onInputChange}
+				/>
+			</form>
+			<div className={modalStyles.buttons}>
+				<Button
+					variant="outlined"
+					onClick={onCloseEvent}
+					style={{ marginRight: "10px" }}
+				>
+					X
+				</Button>
+				<Button variant="contained" onClick={onCreateClick}>
+					만들기
+				</Button>
+			</div>
+		</CustomModal>
+	);
+}
+
+export default TopicModal;
