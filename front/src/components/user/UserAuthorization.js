450ebf7c6f88126bedecdc9cd2d9206513d1eca0
Dellil <aksaksdm@naver.com>
2020-09-22 21:26:31 +0900

    add packages

--- front/src/components/user/UserAuthorization.js
+++ front/src/components/user/UserAuthorization.js
@@ -5,14 +5,18 @@ import { useAuthStyles } from "styles/user";
 import SignUp from "./SignUp";
 import LogIn from "./LogIn";
 
-function UserAuthorization() {
+function UserAuthorization({ setAuth }) {
+	// 쿠키 존재함 변수로 setAuth 설정
+
 	const authStyles = useAuthStyles();
-	const [isLogInOpend, setLogInTrue, setLogInFalse] = useBoolean(true);
+	const [isLogInOpened, setLogInTrue, setLogInFalse] = useBoolean(true);
 
 	return (
 		<div className={authStyles.container}>
-			{isLogInOpend && <LogIn setLogIn={setLogInFalse} isOpend={isLogInOpend} />}
-			{!isLogInOpend && <SignUp setLogIn={setLogInTrue} isOpend={!isLogInOpend} />}
+			{isLogInOpened && <LogIn setLogIn={setLogInFalse} isOpend={isLogInOpened} />}
+			{!isLogInOpened && (
+				<SignUp setLogIn={setLogInTrue} isOpend={!isLogInOpened} />
+			)}
 		</div>
 	);
 }
